f3faadd25f31232030d72eee46a94495fea148d4
Sephora <karkarsephora@gmail.com>
2019-10-24 16:01:15 +0200

    fenetre option

--- src/View/ViewHandler.java
+++ src/View/ViewHandler.java
@@ -21,6 +21,8 @@ public class ViewHandler extends Application {
     private Stage primaryStage;
     private Scene scene, scene2, scene3, scene4;
     private ImageView mario;
+    private ImageView yoshi;
+    private ImageView mario2;
     private ImageView fond1;
     private ImageView fond2;
     private ImageView fond3;
@@ -38,10 +40,12 @@ public class ViewHandler extends Application {
         Group root3 = new Group();
         Group root4 = new Group();
         Scene scene = new Scene(root, 800,500, Color.BLACK);
-        Scene scene2 = new Scene(root2, Color.WHITE);
-        Scene scene3 = new Scene(root3, Color.PINK);
+        Scene scene2 = new Scene(root2, Color.BLACK);
+        Scene scene3 = new Scene(root3, Color.BLACK);
         Scene scene4 = new Scene(root4);
         Text texte = new Text();
+        Text texteM = new Text();
+        Text texteY = new Text();
 
         final File file = new File("src/Asset/Son/mario-theme.mp3");
         final Media media = new Media(file.toURI().toString());
@@ -52,11 +56,24 @@ public class ViewHandler extends Application {
         btn3 = initButton(700,500,"Quitter");
         btn4 = initButton(700,500,"Quitter");
 
-        texte.setText("MARIO-PELLE");
+
+        texte.setText("MARIO DEMINEUR");
         texte.setX(550);
         texte.setY(250);
         texte.setFont(new Font("Indie flower",60));
-        texte.setFill(Color.ORANGE);
+        texte.setFill(Color.RED);
+
+        texteM.setText("MARIO");
+        texteM.setX(750);
+        texteM.setY(750);
+        texteM.setFont(new Font("Indie flower",30));
+        texteM.setFill(Color.RED);
+
+        texteY.setText("YOSHI");
+        texteY.setX(350);
+        texteY.setY(750);
+        texteY.setFont(new Font("Indie flower",30));
+        texteY.setFill(Color.GREEN);
 
         btn1.setOnAction(event -> primaryStage.setScene(scene2));
         btn2.setOnAction(event -> primaryStage.setScene(scene3));
@@ -65,10 +82,12 @@ public class ViewHandler extends Application {
 
         initBackground();
         initBackground2();
-        initPerso();
+        initPerso1();
         initMarche();
         initSaute();
-        initBackground3();
+        //initBackground3();
+        initPerso2();
+        initPerso3();
 
         root.getChildren().addAll(fond1);
         root.getChildren().add(btn1);
@@ -78,7 +97,11 @@ public class ViewHandler extends Application {
         root2.getChildren().addAll(fond2);
         root2.getChildren().add(btn4);
         root2.getChildren().add(mario);
-        root3.getChildren().addAll(fond3);
+        //root3.getChildren().addAll(fond3);
+        root3.getChildren().add(yoshi);
+        root3.getChildren().add(mario2);
+        root3.getChildren().add(texteM);
+        root3.getChildren().add(texteY);
 
         primaryStage.setFullScreen(true);
         primaryStage.setScene(scene);
@@ -92,7 +115,7 @@ public class ViewHandler extends Application {
     }
 
     private void initBackground(){
-        fond1 = new ImageView("Asset/Image/fond1.png");
+        fond1 = new ImageView("Asset/Image/fond3.jpg");
         Rectangle2D primaryScreenBounds = Screen.getPrimary().getBounds();
         fond1.setFitHeight((int) primaryScreenBounds.getHeight());
         fond1.setFitWidth((int) primaryScreenBounds.getWidth());
@@ -113,12 +136,26 @@ public class ViewHandler extends Application {
         return bouton;
     }
 
-    private void initPerso() {
+    private void initPerso1() {
         mario = new ImageView("Asset/Image/mario.gif");
         mario.setX(180);
         mario.setY(630);
         mario.setScaleX(0.2);
         mario.setScaleY(0.2);
+    }
+    private void initPerso2() {
+        yoshi = new ImageView("Asset/Image/yoshi.gif");
+        yoshi.setX(200);
+        yoshi.setY(450);
+        yoshi.setScaleX(0.5);
+        yoshi.setScaleY(0.5);
+    }
+    private void initPerso3() {
+        mario2 = new ImageView("Asset/Image/mario.gif");
+        mario2.setX(600);
+        mario2.setY(500);
+        mario2.setScaleX(0.6);
+        mario2.setScaleY(0.6);
     }
 
     private void initMarche(){
@@ -130,7 +167,7 @@ public class ViewHandler extends Application {
         mario.setOnMouseReleased(event -> mario.setLayoutY(0));
     }
     private void initBackground3(){
-        fond3 = new ImageView("Asset/Image/fond3.jpg");
+        fond3 = new ImageView("Asset/Image/fond1.png");
         Rectangle2D primaryScreenBounds = Screen.getPrimary().getBounds();
         fond3.setFitHeight((int) primaryScreenBounds.getHeight());
         fond3.setFitWidth((int) primaryScreenBounds.getWidth());
