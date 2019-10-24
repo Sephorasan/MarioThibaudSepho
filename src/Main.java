08d7fa99d652968a3a2f5551d287897f512f6bc6
Sephora <karkarsephora@gmail.com>
2019-10-24 09:32:23 +0200

    correction

--- src/Main.java
+++ src/Main.java
@@ -11,27 +11,11 @@ import javafx.scene.image.ImageView;
 
 
 
-public class Main extends Application {
-    private Scene scene, scene2, scene3, scene4;
-    private ImageView mario;
-    private ImageView fond1;
+public class Main{
+
 
 
     public static void main(String[] args){Application.launch(ViewHandler.class, args);}
-
-
-    private void initBackground(){
-    fond1 = new ImageView("Asset/Image/fond1.png");
-    Rectangle2D primaryScreenBounds = Screen.getPrimary().getBounds();
-    fond1.setFitHeight((int) primaryScreenBounds.getHeight());
-    fond1.setFitWidth((int) primaryScreenBounds.getWidth());
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-
-    }
-
 
 
 }
--- src/View/ViewHandler.java
+++ src/View/ViewHandler.java
@@ -1,11 +1,14 @@
 package View;
 
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -13,9 +16,13 @@ import javafx.stage.StageStyle;
 
 public class ViewHandler extends Application {
     private Stage primaryStage;
+    private Scene scene, scene2, scene3, scene4;
+    private ImageView mario;
+    private ImageView fond1;
     private Button btn1;
     private Button btn2;
     private Button btn3;
+
 
     @Override
     public void start(Stage primaryStage){
@@ -25,7 +32,7 @@ public class ViewHandler extends Application {
         Group root2 = new Group();
         Group root3 = new Group();
         Group root4 = new Group();
-        Scene scene = new Scene(root, 800,500, Color.GREEN);
+        Scene scene = new Scene(root, 800,500, Color.BLACK);
         Scene scene2 = new Scene(root2, 800,500, Color.WHITE);
         Scene scene3 = new Scene(root3, 800,500, Color.PINK);
         Scene scene4 = new Scene(root4);
@@ -39,15 +46,31 @@ public class ViewHandler extends Application {
         btn2.setOnAction(event -> primaryStage.setScene(scene3));
         btn3.setOnAction(event -> primaryStage.close());
 
+        initBackground();
 
-        primaryStage.setTitle("Mario");
-        primaryStage.setFullScreenExitHint("");
+        root.getChildren().addAll(fond1);
+        root.getChildren().add(btn1);
+        root.getChildren().add(btn2);
+        root.getChildren().add(btn3);
+        root.getChildren().add(texte);
+
+
         primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
-        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setFullScreen(true);
+        primaryStage.setTitle("Mario");
+        //primaryStage.setFullScreenExitHint("");
+        //primaryStage.setResizable(false);
+        //primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.show();
 
+    }
+
+
+    private void initBackground(){
+        fond1 = new ImageView("Asset/Image/fond1.png");
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getBounds();
+        fond1.setFitHeight((int) primaryScreenBounds.getHeight());
+        fond1.setFitWidth((int) primaryScreenBounds.getWidth());
     }
 
     private Button initButton(int longueur, int largeur, String texteDuBouton) {
